abbf207c6ed805a84527064a843ce14f41b4543f
omar-mhmd <50197421+omar-mhmd@users.noreply.github.com>
2019-05-19 15:53:25 +0300

    Add files via upload

---
+++ Exercises/Javascript-Basics-01/step2.js
@@ -0,0 +1,9 @@
+var a = "Omar";
+
+var b = "Mohammad";
+
+var c = "Beirut";
+
+alert("Name:" + " " + a);
+alert("Surname:" + " " + b);
+alert("City:" + " " + c);
---
+++ Exercises/Javascript-Basics-01/step7.js
@@ -0,0 +1,5 @@
+var a = prompt("Enter your shoe size");
+
+var b = prompt("Enter your birth year");
+
+alert((a * 2) + (5 * 50)- b + 1766 );
