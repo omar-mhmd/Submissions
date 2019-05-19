eef3b415bc02ad75c397aeec933328b8425a2d78
omar-mhmd <mouri700@gmail.com>
2019-05-19 15:22:13 +0300

    step 4

---
+++ Exercises/Javascript-Basics-01/step4.js
@@ -0,0 +1,10 @@
+var a = prompt("Enter your name");
+
+var b = prompt("Enter your surname");
+
+var c = prompt("Enter your city location");
+
+alert("Hello" + " " + a +" " + b);
+alert("your location is in" + " " + c);
+
+
