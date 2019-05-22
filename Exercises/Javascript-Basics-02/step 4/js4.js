751be72ea2f9a871d7f923a50facf0a37be5c812
omar-mhmd <mouri700@gmail.com>
2019-05-22 12:20:38 +0300

    JS basics 2 step 4

---
+++ Exercises/Javascript-Basics-02/step 4/js4.js
@@ -0,0 +1,11 @@
+var a = document.querySelector("button");
+var b = document.querySelector("input");
+a.addEventListener("click",lol);
+function lol () { 
+   var lmao ;
+   if (confirm("yes or no")) {
+       yes=document.getElementById("name").value=" ";
+       document.getElementById("surname").value=" ";
+       document.getElementById("city").value=" ";
+   }
+}
