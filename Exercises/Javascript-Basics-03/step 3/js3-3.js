fd8b94bc6481cc8228683bedebfec7f0d7b4a627
omar-mhmd <mouri700@gmail.com>
2019-05-22 17:32:49 +0300

    js 3 step 3

---
+++ Exercises/Javascript-Basics-03/step 3/js3-3.js
@@ -0,0 +1,21 @@
+var red = document.querySelector(".red");
+var green = document.querySelector(".green");
+var blue = document.querySelector(".blue");
+var batikha = document.getElementById("text");
+red.addEventListener("click", batata)
+
+function batata() {
+    batikha.style.color = "red";
+}
+
+green.addEventListener("click", crispy)
+
+function crispy () {
+    batikha.style.color = "green";
+}
+
+blue.addEventListener("click", pumba) 
+
+function pumba () {
+    batikha.style.color = "blue";
+}
