ac0762b8283b082f3471da3abae55ebb97369398
omar-mhmd <mouri700@gmail.com>
2019-06-11 14:15:43 +0300

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -5,4 +5,12 @@ app.get('/', (req, res) =>{
     res.send('Ok');
 });
 
+app.get('/test', (req, res) => {
+    res.send('{status:200, message:"ok"}');
+})
+
+app.get('/time', (req, res) => {
+    res.send('{status:200, message:14:12 pm}');
+})
+
 app.listen(3000, () => console.log("Listening to port 3000"));
