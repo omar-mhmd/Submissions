6685846e6e94775cc078828cb69367c09d478753
omar-mhmd <mouri700@gmail.com>
2019-06-12 10:21:51 +0300

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -30,4 +30,28 @@ else {
     res.send(`{status:500, error:true, message:you have to provide a search}`)
 }
 });
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.get('/movies/create' , (req, res) => {
+    res.send("okay");
+});
+
+app.get('/movies/read' , (req, res) => {
+    res.send({status:200, data: movies});
+});
+
+app.get('/movies/update' , (req, res) => {
+    res.send("nope");
+});
+
+app.get('/movies/delete' , (req, res) => {
+    res.send("ciao");
+});
+
 app.listen(3000, () => console.log("Listening to port 3000"));
