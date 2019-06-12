04a1f06693908fc32218f790decc871cbd253020
omar-mhmd <mouri700@gmail.com>
2019-06-12 10:11:28 +0300

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -9,8 +9,25 @@ app.get('/test', (req, res) => {
     res.send('{status:200, message:"ok"}');
 })
 
-app.get('/time', (req, res) => {
-    res.send('{status:200, message:14:12 pm}');
-})
+app.get('/time', function(req, res){
+    var time = new Date();
+     res.send({status: 200,
+         message: time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds()
+     })
+ })
 
+app.get('/hello/:ID', (req, res) => {
+    res.send(`{status:200, message:Hello, ${req.params.ID}}`);
+});
+
+app.get('/search', (req, res) => {
+    const m = req.query.m
+    if(m === req.query.m){
+        res.send(`{status:200, message:"ok", data: ${req.query.s}}`);
+}
+
+else {
+    res.send(`{status:500, error:true, message:you have to provide a search}`)
+}
+});
 app.listen(3000, () => console.log("Listening to port 3000"));
