352470eb4405af6fbbf4e7fba6d536f3b3c5a1c1
omar-mhmd <mouri700@gmail.com>
2019-06-14 08:46:36 +0300

    updating.....again

--- Exercises/portfolio/portfolio-app/src/Component/work3.js
+++ Exercises/portfolio/portfolio-app/src/Component/work3.js
@@ -8,6 +8,11 @@ class Work extends React.Component{
             <h2>Work</h2>
         </div>
         
+        <div className="work-text">
+        <p><b>RandomText is a tool designers and developers can use to quickly grab dummy text in either Lorem Ipsum or Gibberish format.</b></p>
+        
+        <p>There are a number of features that make RandomText a little different from other Lorem Ipsum dummy text generators you may find around the web....</p> </div>
+        
         <div>
         <button className="but4">
       <a className="text"type="link" href="main1.html"><h2>Go back to homepage</h2></a>
